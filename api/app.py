8c6199d63a5bfe7917a13a21d54b4e08f8221b37
Siddharth Agarwal <sid.agl77@gmail.com>
2026-04-21 23:39:59 +0530

    Added deployment setup

--- api/app.py
+++ api/app.py
@@ -92,5 +92,11 @@ def search():
     return jsonify([dict(row) for row in products])
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
